0b6c95197c476bbfc17979b7792cffffc9d6377f
Billy Cao <aliencaocao@gmail.com>
2022-02-12 00:36:28 +0800

    Update no embedding and with context

--- with context test.py
+++ with context test.py
@@ -1,5 +1,6 @@
 import gc
 import logging
+
 logging.disable(logging.WARNING)
 import numpy as np
 import pandas as pd
@@ -15,7 +16,7 @@ print(tf.__version__)
 
 # Load data
 # df = pd.read_parquet('E:/train_low_mem.parquet')
-df = pd.read_csv('kaggle/input/ubiquant-market-prediction/train.csv', engine='c')
+df = pd.read_csv('kaggle/input/ubiquant-market-prediction/train.csv', engine='pyarrow')
 df.drop(['row_id'], inplace=True, axis=1)
 investment_id = df['investment_id']
 df = df.groupby(['time_id'])
@@ -35,49 +36,32 @@ print(max(investment_ids))
 print(vocab_size)
 assert len(x) == len(y)
 
+
 def preprocess(x, x_id, y):
     global x_time, width, num_remaining, remaining_slice, rows_to_fill, padded_remaining
     x_time = [tf.convert_to_tensor(s, dtype=tf.float16) for s in x]
     width = x_time[0].shape[1]  # should be 300 (no of features)
-    num_remaining = [s.shape[0]%width for s in x_time]  # remaining rows less than 300
+    num_remaining = [s.shape[0] % width for s in x_time]  # remaining rows less than 300
     remaining_slice = [x_time[i][-num_remaining[i]:] for i in range(len(x_time))]
-    rows_to_fill = [width-r for r in num_remaining]
+    rows_to_fill = [width - r for r in num_remaining]
     padded_remaining = [tf.concat([remaining_slice[i], tf.cast(tf.zeros([rows_to_fill[i], width]), tf.float16)], axis=0) for i in range(len(x_time))]
     x_time = [tf.concat([x_time[i], padded_remaining[i]], axis=0) for i in range(len(x_time))]  # concat padded remaining rows to already split rows
-    x_time = [tf.reshape(s[:-(s.shape[0]%width)], (-1, width, width)) for s in x_time]  # reshape automatically split rows
+    x_time = [tf.reshape(s[:-(s.shape[0] % width)], (width, width, -1)) for s in x_time]  # reshape automatically split rows
     x_time = [[x_t] * len(s) for x_t, s in zip(x_time, x_id)]
     return (x, x_id, x_time), y  # x_time is a list of same images of n x 300 x 300, same across a time step
+
 
 # Pearson correlation coefficient loss and metrics
 def pearson_corr(y_true, y_pred, axis=-1):
     return tfp.stats.correlation(tf.squeeze(y_pred), tf.squeeze(y_true), sample_axis=axis, event_axis=None)
 
+
 def pearson_corr_loss(y_true, y_pred, axis=-1):
     return (1 - pearson_corr(y_true, y_pred, axis=axis)) + 2 * tf.keras.metrics.mean_squared_error(y_true, y_pred)
 
+
 print('Preprocessing dataset')
 ds = preprocess(x, x_id, y)
-
-# def make_dataset(ds, batch_size=64, mode='train'):
-#     single_x = []
-#     single_x_id = []
-#     single_y = []
-#     single_x_time = []
-#     x = ds[0][0]
-#     x_id = ds[0][1]
-#     x_time = ds[0][2]
-#     y = ds[1]
-#     print('Iterating through dataset')
-#     for time_id in range(len(x)):
-#         for sample in x[time_id]:
-#             single_x.append(sample)
-#         for sample in x_id[time_id]:
-#             single_x_id.append(sample)
-#         for sample in y[time_id]:
-#             single_y.append(sample)
-#         single_x_time += [x_time[time_id]] * len(x[time_id])
-#     return (single_x, single_x_id, single_x_time), single_y
-
 print('Done')
 
 print(ds[0][2][-1][-1].shape)
